202136ab1fd0a3c42d6a73409a143c3fe31a7f97
jiang-jin <jiangjinmail@gmail.com>
2015-03-22 16:01:26 +0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -17,18 +17,18 @@ require("data.table")
 require("reshape2")
 
 # Load: activity labels
-activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")[,2]
+activity_labels <- read.table("~/Desktop/WorkDirectory/UCI HAR Dataset/activity_labels.txt")[,2]
 
 # Load: data column names
-features <- read.table("./UCI HAR Dataset/features.txt")[,2]
+features <- read.table("~/Desktop/WorkDirectory/UCI HAR Dataset/features.txt")[,2]
 
 # Extract only the measurements on the mean and standard deviation for each measurement.
 extract_features <- grepl("mean|std", features)
 
 # Load and process X_test & y_test data.
-X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
-y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
-subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+X_test <- read.table("~/Desktop/WorkDirectory/UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("~/Desktop/WorkDirectory/UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("~/Desktop/WorkDirectory/UCI HAR Dataset/test/subject_test.txt")
 
 names(X_test) = features
 
@@ -44,10 +44,10 @@ names(subject_test) = "subject"
 test_data <- cbind(as.data.table(subject_test), y_test, X_test)
 
 # Load and process X_train & y_train data.
-X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
-y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
+X_train <- read.table("~/Desktop/WorkDirectory/UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("~/Desktop/WorkDirectory/UCI HAR Dataset/train/y_train.txt")
 
-subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+subject_train <- read.table("~/Desktop/WorkDirectory/UCI HAR Dataset/train/subject_train.txt")
 
 names(X_train) = features
 
@@ -64,7 +64,6 @@ train_data <- cbind(as.data.table(subject_train), y_train, X_train)
 
 # Merge test and train data
 data = rbind(test_data, train_data)
-
 id_labels   = c("subject", "Activity_ID", "Activity_Label")
 data_labels = setdiff(colnames(data), id_labels)
 melt_data      = melt(data, id = id_labels, measure.vars = data_labels)
